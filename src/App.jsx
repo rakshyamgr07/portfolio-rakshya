3358ca8eed6d30e8bd5e6b4bdb77b463ba044d41
rakshyamgr07 <rakshyaa.97@gmail.com>
2026-07-09 18:29:55 +0545

    frontend files

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h1>Sign Up Form</h1>
+
+      <form>
+        
+      </form>
+    </div>
+  );
+}
+export default App;
+/* <div className="flex justify-center items-center min-h-screen">
+      <form className="border-2 border-black-300 p-2 w-full max-w-xs">
+        <h1 className="text-center font-bold text-green-400 uppercase 
+        lower:scale transition duration-700 cursor-pointer p-3">Contact
+         form</h1>
+        <div className="flex flex-col">
+          <label>Name:</label>
+          <input type="text" placeholder="Full name"></input>
+        </div>
+
+        <div className="flex flex-col">
+          <label>Address:</label>
+          <input type="text" placeholder="permanent address"></input>
+        </div>
+
+        <div className="flex flex-col">
+          <label>Email:</label>
+          <input type="email" placeholder="example@gmail.com"></input>
+        </div>
+
+        <div className="flex flex-col" >
+          <label>Subject:</label>
+          <input type="text" placeholder="subject"></input>
+        </div>
+
+        <div className="flex flex-col"> 
+          <label>Message:</label>
+          <textarea placeholder="your message...."></textarea>
+        </div>
+
+        <div className="flex flex-col ">
+          <button className="text-white-600 bg-green-600 cursor-pointer">send message</button>
+        </div>
+      </form>
+    </div> */
