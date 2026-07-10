314588f1deccfa1f9f2e012a3d70b704884c3f6b
rakshyamgr07 <rakshyaa.97@gmail.com>
2026-07-10 18:55:35 +0545

    second chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 function App() {
+  //hooks
+  const [count,setCount] = useState(0);
   return (
-    <div>
-      <h1>Sign Up Form</h1>
+    // <div className="flex gap-4 items-center justify-center flex-col min-h-screen">
+    //   <h1 >Count:{count}</h1>
+    //   <button className="bg-blue-400 font-bold rounded-sm text-white p-1 hover:scale-105 transition duration-700 rounded-sm" onClick={()=>setCount(count+1)}>Increase</button>
+    
+    // <button className="bg-blue-400 font-bold rounded-sm text-white p-1 hover:scale-105 transition duration-700 rounded-sm" onClick={()=>setCount(count-1)}>Decrease</button>
+ 
+    // </div>
+    <div className=" bg-gray-200 flex gap-4 flex-col items-center  w-70 h-90 border border-black-400 m-10">
+      <h1 className="italic font-bold">Sign Up Form</h1>
 
-      <form>
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col  gap-4 ">
+          <label>Name:</label>
+          <input type="text" placeholder="enter full name" />
+        </div>
+
+        <div className="flex flex-col   gap-4  ">
+          <label>Email:</label>
+          <input type="email" placeholder="example@gmail.com" />
+        </div>
+
+        <div className="flex flex-col   gap-4 ">
+          <label>Address:</label>
+          <input type="text" placeholder="enter permanent address " />
+        </div>
         
+        <button className="m-4 bg-green-500 text-white p-2  rounded-sm w-20 ml-14 hover:scale-105 transition duration-800 ease-in-out cursor-pointer">Sign Up</button>
+      
       </form>
     </div>
   );
 }
-export default App;
+export default App
 /* <div className="flex justify-center items-center min-h-screen">
       <form className="border-2 border-black-300 p-2 w-full max-w-xs">
         <h1 className="text-center font-bold text-green-400 uppercase 
