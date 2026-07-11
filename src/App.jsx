59b430bc060079afbef84ccfe9f7b2fa23da1334
rakshyamgr07 <rakshyaa.97@gmail.com>
2026-07-11 22:10:06 +0545

    Second changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import './App.css';
-function App() {
+function Counter() {
   //hooks
   const [count,setCount] = useState(0);
   return (
-    // <div className="flex gap-4 items-center justify-center flex-col min-h-screen">
-    //   <h1 >Count:{count}</h1>
-    //   <button className="bg-blue-400 font-bold rounded-sm text-white p-1 hover:scale-105 transition duration-700 rounded-sm" onClick={()=>setCount(count+1)}>Increase</button>
+    <div className="bg-gray-200 h-90 w-60 m-10 border border-black-300 flex gap-4 items-center justify-center flex-col">
+      <h1 >Count:{count}</h1>
+      <button className="bg-blue-400 font-bold rounded-sm text-white p-1 hover:scale-105 transition duration-700 rounded-sm" onClick={()=>setCount(count+1)}>Increase</button>
     
-    // <button className="bg-blue-400 font-bold rounded-sm text-white p-1 hover:scale-105 transition duration-700 rounded-sm" onClick={()=>setCount(count-1)}>Decrease</button>
+    <button className="bg-blue-400 font-bold rounded-sm text-white p-1 hover:scale-105 transition duration-700 rounded-sm" onClick={()=>setCount(count-1)}>Decrease</button>
  
-    // </div>
-    <div className=" bg-gray-200 flex gap-4 flex-col items-center  w-70 h-90 border border-black-400 m-10">
+    </div>
+  );
+}
+
+    function Form(){
+      return(
+
+     
+    <div className=" bg-gray-200 flex gap-4 flex-col items-center  w-60 h-90 border border-black-400 m-10">
       <h1 className="italic font-bold">Sign Up Form</h1>
 
       <form className="flex flex-col gap-4">
@@ -36,9 +43,10 @@ function App() {
     </div>
   );
 }
-export default App
-/* <div className="flex justify-center items-center min-h-screen">
-      <form className="border-2 border-black-300 p-2 w-full max-w-xs">
+function Form2(){
+  return(
+     <div className="flex m-10">
+      <form className=" bg-gray-200 border border-black-300 p-2 w-60 h-90">
         <h1 className="text-center font-bold text-green-400 uppercase 
         lower:scale transition duration-700 cursor-pointer p-3">Contact
          form</h1>
@@ -67,8 +75,24 @@ export default App
           <textarea placeholder="your message...."></textarea>
         </div>
 
-        <div className="flex flex-col ">
+        <div className="flex flex-col p-4 ">
           <button className="text-white-600 bg-green-600 cursor-pointer">send message</button>
         </div>
       </form>
-    </div> */
+    </div> 
+  );
+}
+function App(){
+  return(
+      <>
+      <div className="flex flex-rows flex-wrap bg-blue-300">
+      <Counter/>
+      <Form/>
+      <Form2/>
+</div>
+      </>
+  );
+}
+export default App
+// export default Form
+// export default Form2
