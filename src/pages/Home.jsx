02175c61517f94eb3d5ae898465c1f9e58de44a7
rakshyamgr07 <rakshyaa.97@gmail.com>
2026-07-22 19:50:50 +0545

    Second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from './Hero'
 import About from './About'
 
 import Contact from './Contact'
-import Projects from '../components/projects'
+import Projects from '../components/Projects'
 
 function Home() {
   return (
